3c067537c125059b1e5b1a7cd33f588c9d57eaa5
Haqqinurfajri <haqqinur98@gmail.com>
2024-11-03 16:54:49 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -4,18 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menentukan Bilangan Terbesar</title>
+    <title>determine the largest number </title>
 </head>
 
 <body>
     <form action="" method="POST">
-        <label for="bil1">Bilangan Pertama:</label>
+        <label for="bil1">first number:</label>
         <input type="number" name="bil1" id="bil1" required>
         <br>
-        <label for="bil2">Bilangan Kedua:</label>
+        <label for="bil2">second number:</label>
         <input type="number" name="bil2" id="bil2" required>
         <br>
-        <button type="submit" name="submit">Tentukan Bilangan Terbesar</button>
+        <button type="submit" name="submit">determine the largest number </button>
     </form>
 
     <?php
@@ -24,11 +24,11 @@
         $bil2 = intval($_POST['bil2']);
 
         if ($bil1 > $bil2) {
-            echo "<p>Bilangan terbesar adalah: $bil1</p>";
+            echo "<p>the largest number is: $bil1</p>";
         } elseif ($bil2 > $bil1) {
-            echo "<p>Bilangan terbesar adalah: $bil2</p>";
+            echo "<p>the largest number is: $bil2</p>";
         } else {
-            echo "<p>Kedua bilangan sama besar: $bil1</p>";
+            echo "<p>both numbers are the same: $bil1</p>";
         }
     }
     ?>
